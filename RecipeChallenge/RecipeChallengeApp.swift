8cf9594ab955dae458cbbadd77ef83443b9f27c2
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-08-21 23:06:41 -0400

    Initial Commit

---
+++ RecipeChallenge/RecipeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeChallengeApp.swift
+//  RecipeChallenge
+//
+//  Created by Sebastián Rubina on 2024-08-21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
